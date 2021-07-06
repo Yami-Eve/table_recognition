41b35faf2dc9a62a61270116a5f16c9cf1cc8323
Yami-Eve <82312662+Yami-Eve@users.noreply.github.com>
2021-07-06 14:05:48 +0800

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -2,6 +2,6 @@ class HttpConfig {
   static const String baseURL =
       "https://ocrapi-advanced.taobao.com/ocrservice/advanced";
   static const int timeout = 10000;
-  static const String AppKey = "0ae698fc43654335bbb75eaa14b428ed";
+  static const String AppKey = "这里请替换成自己的Key";
   static const String defaultMethod = "GET";
 }
